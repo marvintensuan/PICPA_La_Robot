879f18e582da5a0f6f510062cfd99f9afa84b187
Marvin <mdtensuan_dev@protonmail.com>
2021-02-22 01:04:51 +0800

    Fixed strftime on POST_TITLE. Added TODO.

--- PICPA_La_Robot/main.py
+++ PICPA_La_Robot/main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     continue_to_post = input('Done getting data from Outlook. Should we continue?\n')
 
     if continue_to_post.lower() in ['y', 'yes']:
-        POST_TITLE = f'This is PICPA for the week ending {end_date.strftime()}'
+        POST_TITLE = f"This is PICPA for the week ending {end_date.strftime('%B %d, %Y')}"
         POST_BODY = '# PICPA WEEKLY \nThese are summary of new PICPA Events from' \
             f"{last_week.strftime('%b %d')} to {end_date.strftime('%b %d')}.\n" \
             'You can register in these events at the' \
@@ -53,3 +53,7 @@ if __name__ == '__main__':
         submit_body = reddit.subreddit('testingground4bots').submit(POST_TITLE, POST_BODY)
     else:
         print('I will take that as a NO.')
+
+    # TODO: reddit.validate_on_submit -> set to True
+    # TODO: footer (add new line and break)
+    # TODO: do not use asterisk without escape characters
